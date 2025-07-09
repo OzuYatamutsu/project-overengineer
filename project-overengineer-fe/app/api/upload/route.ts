38cbc2a88735ad30094b684c83c8c50f445a90ad
Sean Lam <seanl@seancotech.com>
2025-07-09 11:28:10 +0900

    First pass at accepting file data from POST

--- project-overengineer-fe/app/api/upload/route.ts
+++ project-overengineer-fe/app/api/upload/route.ts
@@ -1,8 +1,39 @@
 import { NextResponse } from 'next/server'
+import Busboy from 'busboy'
+import { streamToBuffer } from '@/app/api/upload/helpers'
+import { Readable } from 'stream'
 
 export async function POST(request: Request): Promise<NextResponse> {
-  return NextResponse.json(
-    { error: "Not implemented" },
-    { status: 501 }
-  )
+  const bb = Busboy({
+    headers: { 'content-type': request.headers.get('content-type') || ''}
+  })
+  const requestStream = Readable.from(Buffer.from(await request.arrayBuffer()))
+  const fileBufferPromise = new Promise<{
+    data: Buffer
+  }>((resolve, reject) => {
+    let fileBuffer: Buffer
+
+    bb.on('file', (_name, file, _info) => {
+      streamToBuffer(file).then(buffer => {
+        fileBuffer = buffer
+      })
+    })
+
+    // bb.on('data', (chunk) => {chunk.length})
+
+    bb.on('close', () => {
+      resolve({ data: fileBuffer })
+    })
+    bb.on('error', reject)
+    requestStream.pipe(bb)
+  })
+
+  const { data } = await fileBufferPromise
+
+  return NextResponse.json({
+    message: 'Got your image data!',
+    data,
+    size: data.length,
+    status: 201
+  })
 }

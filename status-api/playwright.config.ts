41e4ff76b73348a9bccb127af5d6fc32f5d6e2bb
Sean Lam <seanl@seancotech.com>
2025-07-23 19:33:09 +0800

    ci: fix issue with playwright starting status-api

--- status-api/playwright.config.ts
+++ status-api/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   webServer: {
-    command: 'node dist/server.js',  // Or however you start the WS server
+    command: 'npx ts-node server.ts',
     port: 3001,
     reuseExistingServer: !process.env.CI,
   },

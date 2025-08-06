bbf425d8c1eb8f0c5a6af42ffd4186721cd8b17a
Sean Lam <seanl@seancotech.com>
2025-08-06 23:35:41 +0200

    Add missing config vals

--- status-api/playwright.config.ts
+++ status-api/playwright.config.ts
@@ -5,11 +5,10 @@ export default defineConfig({
     command: 'node dist/server.js',
     reuseExistingServer: false,
     env: {
-      REDIS_HOST: 'localhost',
-      REDIS_PORT: '6379',
-      REDIS_PASSWORD: 'b4yscx92yksfyv9c',
       SENTINEL_HOST: 'localhost',
-      SENTINEL_PORT: '26379'
+      SENTINEL_PORT: '26379',
+      REDIS_PASSWORD: 'b4yscx92yksfyv9c',
+      STATUS_API_PORT: '3001'
     },
     stdout: 'pipe',
     stderr: 'pipe'

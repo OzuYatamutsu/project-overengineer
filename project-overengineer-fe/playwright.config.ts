f74e3c768d0fb87c8b32334c2e52f7d2b520ef91
Sean Lam <seanl@seancotech.com>
2025-08-06 13:53:15 +0200

    Enable logging on playwright test

--- project-overengineer-fe/playwright.config.ts
+++ project-overengineer-fe/playwright.config.ts
@@ -2,9 +2,18 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   webServer: {
-    command: 'pnpm dev', // or `npm run dev`
+    command: 'rm -rf .next && pnpm dev',
     url: 'http://localhost:3000',
-    reuseExistingServer: true, // speeds up re-runs
+    reuseExistingServer: false,
+    env: {
+      REDIS_HOST: 'localhost',
+      REDIS_PORT: '6379',
+      REDIS_PASSWORD: 'b4yscx92yksfyv9c',
+      SENTINEL_HOST: 'localhost',
+      SENTINEL_PORT: '26379'
+    },
+    stdout: 'pipe',
+    stderr: 'pipe'
   },
   use: {
     baseURL: 'http://localhost:3000',

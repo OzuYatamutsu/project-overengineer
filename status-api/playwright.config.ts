b838bd87b80e263044bc9a07dc44e0aff596bdf2
Sean Lam <seanl@seancotech.com>
2025-08-06 22:54:42 +0200

    Port playwright config env changes to status-api unit tests

--- status-api/playwright.config.ts
+++ status-api/playwright.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   webServer: {
     command: 'node dist/server.js',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: false,
+    env: {
+      REDIS_HOST: 'localhost',
+      REDIS_PORT: '6379',
+      REDIS_PASSWORD: 'b4yscx92yksfyv9c',
+      SENTINEL_HOST: 'localhost',
+      SENTINEL_PORT: '26379'
+    },
+    stdout: 'pipe',
+    stderr: 'pipe'
   },
 })

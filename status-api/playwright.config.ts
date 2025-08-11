e750f9578f5ef3091702729c70d4bfdb28cb38f1
Sean Lam <seanl@seancotech.com>
2025-08-11 13:53:27 +0200

    ci: make sure env overrides apply for test

--- status-api/playwright.config.ts
+++ status-api/playwright.config.ts
@@ -5,10 +5,11 @@ export default defineConfig({
     command: 'node dist/server.js',
     reuseExistingServer: false,
     env: {
-      REDIS_HOST: 'localhost',
-      REDIS_PORT: '6379',
-      REDIS_PASSWORD: 'b4yscx92yksfyv9c',
-      STATUS_API_PORT: '3001'
+      ...process.env,
+      REDIS_HOST: process.env.REDIS_HOST || 'localhost',
+      REDIS_PORT: process.env.REDIS_PORT || '6379',
+      REDIS_PASSWORD: process.env.REDIS_PASSWORD || 'b4yscx92yksfyv9c',
+      STATUS_API_PORT: process.env.STATUS_API_PORT || '3001'
     },
     stdout: 'pipe',
     stderr: 'pipe'

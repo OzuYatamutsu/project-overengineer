f00829faba12b26d400320594ee42e2ff2105701
Sean Lam <seanl@seancotech.com>
2025-08-22 19:13:04 +0200

    Add new shared-lib project and npm7 workspace

---
+++ shared-lib/verify.ts
@@ -0,0 +1,8 @@
+import assert from "assert";
+
+export function enforceConfig(configKey: string, print=false): void {
+    assert(!!process.env[configKey], `The required env var ${configKey} wasn't set!`)
+    if (print) {
+        console.log(`config: ${configKey}=${process.env[configKey]}`)
+    }
+}

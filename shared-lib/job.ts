b0a65a835d3cbed7b697669621f57511530f17e0
Sean Lam <seanl@seancotech.com>
2025-08-22 19:13:04 +0200

    Add new shared-lib project and npm7 workspace

---
+++ shared-lib/job.ts
@@ -0,0 +1,33 @@
+import { v4 as uuidv4 } from 'uuid'
+import { JobStatus } from './job-status'
+
+export class Job {
+    public id: string
+    public status: JobStatus
+    public imageDataBase64: string
+    public result: string
+
+    constructor(imageDataBase64: string) {
+        this.id = uuidv4()
+        this.status = JobStatus.NEW
+        this.imageDataBase64 = imageDataBase64
+        this.result = ""
+    }
+
+    static fromRedisObject(redisObject: Record<string, string>): Job {
+        const job = new Job(redisObject.imageDataBase64)
+        job.id = redisObject.id
+        job.status = redisObject.status as JobStatus
+
+        return job
+    }
+
+    serialize(): Record<string, string> {
+        return {
+            id: this.id,
+            status: this.status.toString(),
+            imageDataBase64: this.imageDataBase64,
+            result: this.result
+        }
+    }
+}
---
+++ shared-lib/worker-state.ts
@@ -0,0 +1,4 @@
+export enum WorkerState {
+    IDLE = "IDLE",
+    PROCESSING = "PROCESSING"
+}

6e1d48b3a651831d3c60940a949c1706f6225930
Sean Lam <seanl@seancotech.com>
2025-08-26 22:13:18 +0200

    janitor: refactor code to make it more testable

--- janitor/worker.ts
+++ janitor/worker.ts
@@ -3,25 +3,32 @@ import { getRedis } from '@project-overengineer/shared-lib/redis';
 const POLLING_PERIOD_MSECS = 300000
 const JOB_TTL_SECS = 3600
 
+async function jobIsStale(jobKey: string): Promise<boolean> {
+    const createUtime = Number(
+        await getRedis().hget(jobKey, "createUtime")
+    )
+    
+    if (Number.isNaN(createUtime)) {
+        console.log(`janitor: Job ${jobKey} has an invalid create timestamp, discarding`)
+        return true
+    }
+
+    if ((new Date().getTime() - JOB_TTL_SECS) > createUtime) {
+        console.log(`janitor: Job ${jobKey} is completed or stale, discarding`)
+        return true
+    }
+
+    return false
+}
+
 // poll redis for new jobs
 setInterval(async () => {
     console.log("janitor: starting cleanup job")
-
     const keys = await getRedis().keys(`job:*`)
 
     for (const key of keys) {
-        const createUtime = Number(await getRedis().hget(key, "createUtime"))
-        
-        if (Number.isNaN(createUtime)) {
-            console.log(`janitor: Job ${key} has an invalid create timestamp, discarding`)
+        if (await jobIsStale(key)) {
             getRedis().del(key)
-            break
-        }
-
-        if ((new Date().getTime() - JOB_TTL_SECS) > createUtime) {
-            console.log(`janitor: Job ${key} is completed or stale, discarding`)
-            getRedis().del(key)
-            break
         }
     }
 

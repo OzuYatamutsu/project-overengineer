169b51b7ffc93062f5d37187ff0a978ffb933399
Sean Lam <seanl@seancotech.com>
2025-08-26 22:00:54 +0200

    janitor: add unittest skeleton files

---
+++ janitor/playwright.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from '@playwright/test'
+
+export default defineConfig({
+  webServer: {
+    command: 'TS_NODE_TRANSPILE_ONLY=1 ts-node server.ts',
+    reuseExistingServer: false,
+    env: {
+      ...process.env,
+      REDIS_HOST: process.env.REDIS_HOST || 'localhost',
+      REDIS_PORT: process.env.REDIS_PORT || '6379',
+      REDIS_PASSWORD: process.env.REDIS_PASSWORD || 'b4yscx92yksfyv9c'
+    },
+    stdout: 'pipe',
+    stderr: 'pipe'
+  },
+})

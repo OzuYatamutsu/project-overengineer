844bbe822b3b4738ac06da44570525553df12dce
Sean Lam <seanl@seancotech.com>
2025-08-26 22:05:46 +0200

    janitor: add all unittest skeleton, ci, and vscode launch

--- janitor/playwright.config.ts
+++ janitor/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   webServer: {
-    command: 'TS_NODE_TRANSPILE_ONLY=1 ts-node server.ts',
+    command: 'TS_NODE_TRANSPILE_ONLY=1 ts-node worker.ts',
     reuseExistingServer: false,
     env: {
       ...process.env,
---
+++ janitor/tests/server.test.ts
@@ -0,0 +1,5 @@
+import { test } from '@playwright/test'
+
+test('janitor should remove stale jobs', async () => {})
+test('janitor should not remove non-stale jobs', async () => {})
+test('janitor should remove completed old jobs and not remove active jobs', async () => {})

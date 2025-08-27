514ae7a31469d9db7e998930934264e26b6a4463
Sean Lam <seanl@seancotech.com>
2025-08-27 21:25:23 +0200

    Fix calls to async helpers in rate-limit.test.ts

--- shared-lib/tests/rate-limit.test.ts
+++ shared-lib/tests/rate-limit.test.ts
@@ -2,22 +2,22 @@ import { rateLimit } from '../rate-limit'
 import { test, expect } from '@playwright/test'
 const TEST_IP = '127.0.0.1'
 
-test('jobs arent rate limited when they shouldnt be', () => {
+test('jobs arent rate limited when they shouldnt be', async () => {
   const MAX_CONNECTIONS = 5
   const WITHIN_SECS = 300
 
   for (let i = 0; i < MAX_CONNECTIONS; i++) {
-    expect(rateLimit(TEST_IP, MAX_CONNECTIONS, WITHIN_SECS)).toBeTruthy()
+    expect(await rateLimit(TEST_IP, MAX_CONNECTIONS, WITHIN_SECS)).toBeTruthy()
   }
 })
-test('jobs are rate limited when they should be', () => {
+test('jobs are rate limited when they should be', async () => {
   const MAX_CONNECTIONS = 5
   const WITHIN_SECS = 300
 
   for (let i = 0; i < MAX_CONNECTIONS; i++) {
-    expect(rateLimit(TEST_IP, MAX_CONNECTIONS, WITHIN_SECS)).toBeTruthy()
+    expect(await rateLimit(TEST_IP, MAX_CONNECTIONS, WITHIN_SECS)).toBeTruthy()
   }
   for (let i = 0; i < MAX_CONNECTIONS; i++) {
-    expect(rateLimit(TEST_IP, MAX_CONNECTIONS, WITHIN_SECS)).toBeFalsy()
+    expect(await rateLimit(TEST_IP, MAX_CONNECTIONS, WITHIN_SECS)).toBeFalsy()
   }
 })

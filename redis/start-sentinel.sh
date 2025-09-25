db933fc2fed43f3a49277c0da6827b965e1b6821
Sean Lam <seanl@seancotech.com>
2025-09-25 21:40:26 +0100

    cd: generate sentinel config at runtime instead of build time

---
+++ redis/start-sentinel.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Inject redis-sentinel runtime variables and start sentinel.
+set -e
+
+cat > /etc/sentinel.conf <<EOF
+bind 0.0.0.0
+port ${SENTINEL_PORT}
+sentinel monitor redis-master ${REDIS_HOST} ${REDIS_PORT} 2
+$( [ -n "$REDIS_PASSWORD" ] && echo "sentinel auth-pass redis-master ${REDIS_PASSWORD}" )
+sentinel down-after-milliseconds redis-master 5000
+sentinel parallel-syncs redis-master 1
+sentinel failover-timeout redis-master 10000
+sentinel resolve-hostnames yes
+EOF
+
+exec redis-sentinel /etc/sentinel.conf

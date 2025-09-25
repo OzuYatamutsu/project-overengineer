ca00b87dcd97b4571ef16e6c03dcc2f2f24c0c35
Sean Lam <seanl@seancotech.com>
2025-09-25 21:48:36 +0100

    Fix ro filesystem error

--- redis/start-sentinel.sh
+++ redis/start-sentinel.sh
@@ -2,7 +2,7 @@
 # Inject redis-sentinel runtime variables and start sentinel.
 set -e
 
-cat > /etc/sentinel.conf <<EOF
+cat > /tmp/sentinel.conf <<EOF
 bind 0.0.0.0
 port ${SENTINEL_PORT}
 sentinel monitor redis-master ${REDIS_HOST} ${REDIS_PORT} 2
@@ -13,4 +13,4 @@ sentinel failover-timeout redis-master 10000
 sentinel resolve-hostnames yes
 EOF
 
-exec redis-sentinel /etc/sentinel.conf
+exec redis-sentinel /tmp/sentinel.conf

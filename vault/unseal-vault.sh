cbaafb03da3c0ac642a54039fd108af0afc3bcad
Sean Lam <seanl@seancotech.com>
2025-10-18 14:20:01 -0700

    Write unseal data to /tmp instead

--- vault/unseal-vault.sh
+++ vault/unseal-vault.sh
@@ -15,15 +15,18 @@ done
 # Initialize Vault if not already initialized
 if ! vault status -address=https://vault-0.svc-vault.default.svc.cluster.local:8200 -tls-skip-verify | grep -q 'Initialized.*true'; then
   echo "Initializing Vault..."
-  vault operator init -address=https://vault-0.svc-vault.default.svc.cluster.local:8200 -key-shares=1 -key-threshold=1 -format=json -tls-skip-verify > /vault/unseal-info.json
-  UNSEAL_KEY=$(jq -r '.unseal_keys_b64[0]' /vault/unseal-info.json)
-  ROOT_TOKEN=$(jq -r '.root_token' /vault/unseal-info.json)
+  vault operator init -address=https://vault-0.svc-vault.default.svc.cluster.local:8200 -key-shares=1 -key-threshold=1 -format=json -tls-skip-verify > /tmp/vault-unseal-info.json
+  UNSEAL_KEY=$(jq -r '.unseal_keys_b64[0]' /tmp/vault-unseal-info.json)
+  ROOT_TOKEN=$(jq -r '.root_token' /tmp/vault-unseal-info.json)
 
   echo "Unsealing Vault..."
   vault operator unseal -address=https://vault-0.svc-vault.default.svc.cluster.local:8200 -tls-skip-verify $UNSEAL_KEY
 
   echo "Saving credentials to a secret..."
-  kubectl create secret generic vault-init-keys --from-file=/vault/unseal-info.json --dry-run=client -o yaml | kubectl apply -f -
+  kubectl create secret generic vault-init-keys --from-file=/tmp/vault-unseal-info.json --dry-run=client -o yaml | kubectl apply -f -
+
+  rm -f /tmp/vault-unseal-info.json
+  echo "Vault initialization complete."
 else
   echo "Vault already initialized."
 fi

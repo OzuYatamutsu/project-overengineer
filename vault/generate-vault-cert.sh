c191428d2a0483f016d236cad634011cfc4bfb4a
Sean Lam <seanl@seancotech.com>
2025-10-21 14:40:20 -0700

    Fix CA cert generation

--- vault/generate-vault-cert.sh
+++ vault/generate-vault-cert.sh
@@ -9,7 +9,7 @@ openssl req -x509 -new -nodes -days 36135 -config vault/vault-openssl-ca.conf -k
 openssl req -new -nodes -config vault/vault-openssl.conf -keyout vault/vault.key -out vault/vault.csr
 
 # Sign the CSR
-openssl x509 -req -in vault/vault.csr -CA vault/vault-ca.crt -CAkey vault/vault-ca.key -CAcreateserial -out vault/vault.crt -days 36135 -sha256 extfile vault/vault-openssl.conf -extensions req_ext
+openssl x509 -req -in vault/vault.csr -CA vault/vault-ca.crt -CAkey vault/vault-ca.key -CAcreateserial -out vault/vault.crt -days 36135 -sha256 -extfile vault/vault-openssl.conf -extensions req_ext
 
 # Verify
-openssl x509 -in vault.crt -noout -text
+openssl x509 -in vault/vault.crt -noout -text

fd0340be8e6d1b02319c30227848d5dc808b45ef
Sean Lam <seanl@seancotech.com>
2025-10-25 00:28:52 -0700

    Update env config value instead of returning value from vault

--- shared-lib/tests/vault.test.ts
+++ shared-lib/tests/vault.test.ts
@@ -1,14 +1,24 @@
 import { test, expect } from '@playwright/test'
-import { getValueFromVault, getVault, watchAndUpdateVaultValue } from '../vault'
+import { updateEnvFromVault, getVault, watchAndUpdateVaultValue } from '../vault'
 
 test('can get a vault object', () => {
   expect(getVault("shared-lib")).toBeTruthy()
 })
 
-test('can get a value from vault', () => {
-  expect(getValueFromVault("shared-lib", "_TEST_CONFIG_VALUE")).toBeTruthy()
+test('can update an env from vault', () => {
+  const initialValue = "VALUE1"
+  process.env["_TEST_CONFIG_VALUE"] = initialValue
+
+  expect(process.env["_TEST_CONFIG_VALUE"]).toBeTruthy()
+  expect(process.env["_TEST_CONFIG_VALUE"]).toBe(initialValue)
+
+  updateEnvFromVault("shared-lib", "_TEST_CONFIG_VALUE")
+
+  expect(process.env["_TEST_CONFIG_VALUE"]).toBeTruthy()
+  expect(process.env["_TEST_CONFIG_VALUE"]).not.toBe(initialValue)
 })
 
 test('can start watching and updating a value from vault', () => {
+  // Just test if we can create a background job
   expect(watchAndUpdateVaultValue("shared-lib", "_TEST_CONFIG_VALUE")).toBeTruthy()
 })
--- shared-lib/vault.ts
+++ shared-lib/vault.ts
@@ -1,18 +1,24 @@
-import assert from "assert";
-import { enforceConfig } from "./verify";
+import assert from "assert"
+import { enforceConfig } from "./verify"
+import { log } from "./logging"
 
 export function getVault(serviceName: string) {
     assert(false, "not implemented")
 }
 
-export async function getValueFromVault(serviceName: string, configName: string): Promise<string> {
+export async function updateEnvFromVault(serviceName: string, configName: string): Promise<void> {
     enforceConfig(serviceName, "VAULT_HOST")
     enforceConfig(serviceName, "VAULT_PORT")
 
-    assert(false, "not implemented")
-    return ""
+    const freshValue = ""  // TODO, implement getting value from vault
+
+    if (freshValue != process.env[configName]) {
+        log(serviceName, `writing new config value for: ${configName}`)
+    }
+    process.env[configName] = freshValue
+    assert(false, "not implemented")  // TODO
 }
 
 export function watchAndUpdateVaultValue(serviceName: string, configName: string, refreshPeriodMs=60000): void {
-    setInterval(async () => await getValueFromVault(serviceName, configName), refreshPeriodMs)
+    setInterval(async () => await updateEnvFromVault(serviceName, configName), refreshPeriodMs)
 }

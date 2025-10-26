2ddb5054029a8bd4be496893149e6fb495563873
Sean Lam <seanl@seancotech.com>
2025-10-26 22:27:54 +0800

    More work implementing vault at runtime

--- shared-lib/vault.ts
+++ shared-lib/vault.ts
@@ -1,22 +1,37 @@
 import assert from "assert"
+import vault from "node-vault"
 import { enforceConfig } from "./verify"
 import { log } from "./logging"
 
-export function getVault(serviceName: string) {
-    assert(false, "not implemented")
-}
+const CONFIG_PREFIX = ""
 
-export async function updateEnvFromVault(serviceName: string, configName: string): Promise<void> {
+let vaultClient: vault.client
+
+export async function getVault(serviceName: string) {
     enforceConfig(serviceName, "VAULT_HOST")
     enforceConfig(serviceName, "VAULT_PORT")
 
-    const freshValue = ""  // TODO, implement getting value from vault
+    if (!vaultClient) {
+        log(serviceName, `opening new vault connection to https://${process.env.VAULT_HOST}:${process.env.VAULT_PORT}`)
+
+        vaultClient = vault({
+            apiVersion: "v1",
+            endpoint: `https://${process.env.VAULT_HOST}:${process.env.VAULT_PORT}`
+            // token: process.env.VAULT_TOKEN
+            // TODO auth
+        })
+    }
+    
+    return vaultClient
+}
+
+export async function updateEnvFromVault(serviceName: string, configName: string): Promise<void> {
+    const freshValue = await (await getVault(serviceName)).read(`config/${configName}`)
 
     if (freshValue != process.env[configName]) {
-        log(serviceName, `writing new config value for: ${configName}`)
+        log(serviceName, `updating config value from vault: ${configName}`)
     }
     process.env[configName] = freshValue
-    assert(false, "not implemented")  // TODO
 }
 
 export function watchAndUpdateVaultValue(serviceName: string, configName: string, refreshPeriodMs=60000): void {

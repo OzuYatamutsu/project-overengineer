611f76da57a275014656bd54db973699e04560a7
Sean Lam <seanl@seancotech.com>
2025-10-29 19:22:11 +0800

    Add missing test configs

--- status-api/playwright.config.ts
+++ status-api/playwright.config.ts
@@ -9,7 +9,10 @@ export default defineConfig({
       REDIS_HOST: process.env.REDIS_HOST || 'localhost',
       REDIS_PORT: process.env.REDIS_PORT || '6379',
       REDIS_PASSWORD: process.env.REDIS_PASSWORD || 'b4yscx92yksfyv9c',
-      STATUS_API_PORT: process.env.STATUS_API_PORT || '3001'
+      STATUS_API_PORT: process.env.STATUS_API_PORT || '3001',
+      VAULT_HOST: process.env.STATUS_API_PORT || 'localhost',
+      VAULT_PORT: process.env.VAULT_PORT || '8200',
+      VAULT_RO_TOKEN: process.env.VAULT_RO_TOKEN || 'root'
     },
     stdout: 'pipe',
     stderr: 'pipe'

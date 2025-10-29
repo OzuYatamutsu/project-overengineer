c8ec33f663dc71cf51e0347e9b304b339538fbf6
Sean Lam <seanl@seancotech.com>
2025-10-29 19:22:04 +0800

    Add missing test configs

--- janitor/playwright.config.ts
+++ janitor/playwright.config.ts
@@ -8,7 +8,10 @@ export default defineConfig({
       ...process.env,
       REDIS_HOST: process.env.REDIS_HOST || 'localhost',
       REDIS_PORT: process.env.REDIS_PORT || '6379',
-      REDIS_PASSWORD: process.env.REDIS_PASSWORD || 'b4yscx92yksfyv9c'
+      REDIS_PASSWORD: process.env.REDIS_PASSWORD || 'b4yscx92yksfyv9c',
+      VAULT_HOST: process.env.STATUS_API_PORT || 'localhost',
+      VAULT_PORT: process.env.VAULT_PORT || '8200',
+      VAULT_RO_TOKEN: process.env.VAULT_RO_TOKEN || 'root'
     },
     stdout: 'pipe',
     stderr: 'pipe'

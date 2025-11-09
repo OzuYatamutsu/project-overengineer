9c8b29ad51fc737786a30690281868702f1593b4
Sean Lam <seanl@seancotech.com>
2025-11-09 23:24:53 +0900

    Debug

--- shared-lib/vault.ts
+++ shared-lib/vault.ts
@@ -51,8 +51,8 @@ export async function getValue(serviceName: string, configName: string, insecure
         await (await getVault(serviceName, insecure)).read(`${CONFIG_PREFIX}/${configName}`)
     )
 
-    log(serviceName, `debug: ${result}`)
-    log(serviceName, `debug: ${result.data}`)
+    log(serviceName, `debug: ${JSON.stringify(result)}`)
+    log(serviceName, `debug: ${JSON.stringify(result.data)}`)
     log(serviceName, `debug: ${result.data.data}`)
     return result.data.data.value
 }

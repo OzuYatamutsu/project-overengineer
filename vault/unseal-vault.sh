946031e665785309cd66b61fcd3ff32386f62b3e
Sean Lam <seanl@seancotech.com>
2025-11-27 23:27:32 -0800

    Generate image encryption key

--- vault/unseal-vault.sh
+++ vault/unseal-vault.sh
@@ -108,6 +108,10 @@ while true; do
     echo "Renewing vault read token..."
     vault token renew -address="$VAULT_ADDR" "$RO_KEY"
 
+    echo "Generating image encryption key..."
+    vault kv put -address="$VAULT_ADDR" secret/data/IMAGE_KEY value="$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)"
+    echo "Image encryption key saved."
+
     echo "Done. Sleeping..."
     sleep 1800
   else

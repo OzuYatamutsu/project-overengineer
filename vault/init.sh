28ff17a1546adedf3ec26b43854141c9996b9ce1
Sean Lam <seanl@seancotech.com>
2025-11-29 18:34:41 -0500

    Determine primary state in init script

--- vault/init.sh
+++ vault/init.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 set -e
 
-/bin/sh /vault/set-primary-state.sh  # Generates state.sh
-source /vault/state.sh
+# First, determine primary state
+IS_PRIMARY=""
+echo "Setting primary state..."
+ready=$(kubectl get statefulset vault -o jsonpath='{.status.readyReplicas}' || echo 0)
+if [ "${ready:-0}" -ge 2 ]; then
+  echo "This looks like a secondary."
+  IS_PRIMARY=false
+else
+  echo "This looks like a primary."
+  IS_PRIMARY=true
+fi
 
-/bin/sh /vault/generate-pw.sh
-/bin/sh /vault/unseal-vault.sh
+# Then run all other init scripts
+IS_PRIMARY=$IS_PRIMARY /bin/sh /vault/generate-pw.sh
+IS_PRIMARY=$IS_PRIMARY /bin/sh /vault/unseal-vault.sh
 if [ "$IS_PRIMARY" = true ]; then
-  /bin/sh /vault/renew-keys.sh
+  IS_PRIMARY=$IS_PRIMARY /bin/sh /vault/renew-keys.sh
 fi

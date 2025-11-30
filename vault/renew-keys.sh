ea6090877d745208022f4d7ef8adf1dcbedc3fec
Sean Lam <seanl@seancotech.com>
2025-11-29 21:12:59 -0500

    Don't save unseal info to disk during renew-keys.sh

--- vault/renew-keys.sh
+++ vault/renew-keys.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
-kubectl get secret vault-init-keys -o jsonpath='{.data.vault-unseal-info\.json}' | base64 -d > /vault/data/vault-unseal-info.json
 VAULT_ADDR="https://svc-vault.default.svc.cluster.local:8200"
-UNSEAL_KEY=$(jq -r '.unseal_keys_b64[0]' /vault/data/vault-unseal-info.json)
-ROOT_TOKEN=$(jq -r '.root_token' /vault/data/vault-unseal-info.json)
+UNSEAL_KEY=$(kubectl get secret vault-init-keys -o jsonpath='{.data.vault-unseal-info\.json}' | base64 -d | jq -r '.unseal_keys_b64[0]')
+ROOT_TOKEN=$(kubectl get secret vault-init-keys -o jsonpath='{.data.vault-unseal-info\.json}' | base64 -d | jq -r '.root_token')
 RO_KEY=$(kubectl get secret vault-ro-token -o jsonpath='{.data.token}' | base64 -d)
-rm /vault/data/vault-unseal-info.json
 
 vault login -address="$VAULT_ADDR" $ROOT_TOKEN
 

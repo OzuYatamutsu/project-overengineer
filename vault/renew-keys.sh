f97441337f600f9eaafb8b442c6fc1911811ae52
Sean Lam <seanl@seancotech.com>
2025-11-29 20:58:44 -0500

    Remove extra whitespace

--- vault/renew-keys.sh
+++ vault/renew-keys.sh
@@ -15,7 +15,7 @@ while true; do
 
   echo "(Re)generating image encryption key..."
   vault kv put -address="$VAULT_ADDR" secret/data/IMAGE_KEY value="$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)"
-  
+
   echo "Done. Sleeping..."
   sleep 1800
 done

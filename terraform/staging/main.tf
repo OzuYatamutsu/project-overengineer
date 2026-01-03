0c4e6c6ce70d27fe5361bf567fdaf1664b39d9f5
Sean Lam <seanl@seancotech.com>
2026-01-03 13:42:39 -0500

    Start deploying OKE resources to OCI (#124)

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -14,7 +14,45 @@ provider "oci" {
 
 resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
-  cidr_block     = "172.16.0.0/20"
+  cidr_block     = "172.16.0.0/24"
   compartment_id = "ocid1.tenancy.oc1..aaaaaaaasz4c4v6umng7swqlgy5leajulguixsgloinsvcciub2hrsqp2jya"
   display_name   = "project-overengineer-vcn"
+}
+
+resource "oci_core_subnet" "staging" {
+  vcn_id                     = oci_core_vcn.internal.id
+  cidr_block                 = "172.16.0.0/24"
+  compartment_id             = "ocid1.tenancy.oc1..aaaaaaaasz4c4v6umng7swqlgy5leajulguixsgloinsvcciub2hrsqp2jya"
+  display_name               = "project-overengineer-subnet-1"
+  prohibit_public_ip_on_vnic = true
+  dns_label                  = "staging"
+}
+
+resource "oci_containerengine_cluster" "oke" {
+  name               = "staging"
+  compartment_id     = oci_core_vcn.internal.compartment_id
+  vcn_id             = oci_core_vcn.internal.id
+  kubernetes_version = "v1.34.1"
+
+  endpoint_config {
+    is_public_ip_enabled = true
+    subnet_id            = oci_core_subnet.staging.id
+  }
+}
+
+resource "oci_containerengine_node_pool" "staging" {
+  cluster_id         = oci_containerengine_cluster.oke.id
+  name               = "staging-pool"
+  compartment_id     = oci_core_vcn.internal.compartment_id
+  kubernetes_version = oci_containerengine_cluster.oke.kubernetes_version
+
+  node_shape = "VM.Standard.A1.Flex"
+
+  node_shape_config {
+    ocpus         = 1
+    memory_in_gbs = 6
+  }
+
+  quantity_per_subnet = 1
+  subnet_ids          = [oci_core_subnet.staging.id]
 }

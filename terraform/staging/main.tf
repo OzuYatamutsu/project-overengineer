693672c325ddde7da1993e79e9a81c2d8c0fa115
Sean Lam <seanl@seancotech.com>
2026-01-03 14:12:17 -0500

    Attempt to provision node pool

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -24,7 +24,7 @@ resource "oci_core_subnet" "staging" {
   cidr_block                 = "172.16.0.0/24"
   compartment_id             = "ocid1.tenancy.oc1..aaaaaaaasz4c4v6umng7swqlgy5leajulguixsgloinsvcciub2hrsqp2jya"
   display_name               = "project-overengineer-subnet-1"
-  prohibit_public_ip_on_vnic = true
+  prohibit_public_ip_on_vnic = false
   dns_label                  = "staging"
 }
 
@@ -41,18 +41,21 @@ resource "oci_containerengine_cluster" "oke" {
 }
 
 resource "oci_containerengine_node_pool" "staging" {
-  cluster_id         = oci_containerengine_cluster.oke.id
-  name               = "staging-pool"
-  compartment_id     = oci_core_vcn.internal.compartment_id
-  kubernetes_version = oci_containerengine_cluster.oke.kubernetes_version
+  cluster_id          = oci_containerengine_cluster.oke.id
+  name                = "staging-pool"
+  compartment_id      = oci_core_vcn.internal.compartment_id
+  kubernetes_version  = oci_containerengine_cluster.oke.kubernetes_version
+  node_shape          = "VM.Standard.A1.Flex"
+  quantity_per_subnet = 1
+  subnet_ids          = [oci_core_subnet.staging.id]
 
-  node_shape = "VM.Standard.A1.Flex"
+  node_source_details {
+    source_type = "image"
+    image_id    = "ocid1.image.oc1.ca-toronto-1.aaaaaaaat7k6c5lxdfydhcj64y5klm75yc4zu2xf7fbqevk64k3y7cjaaxzq"
+  }
 
   node_shape_config {
     ocpus         = 1
     memory_in_gbs = 6
   }
-
-  quantity_per_subnet = 1
-  subnet_ids          = [oci_core_subnet.staging.id]
 }

170b9cb0ca46bc1fe13c944302ca9bd92fb2775d
Sean Lam <seanl@seancotech.com>
2026-01-20 20:36:12 -0800

    Attempt to fix iam dual import issue on terraform apply

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -26,14 +26,8 @@ data "aws_iam_policy" "ebs_csi_policy" {
 }
 
 # GitHub OIDC Provider
-resource "aws_iam_openid_connect_provider" "github" {
+data "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = ["sts.amazonaws.com"]
-
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
 }
 
 # IAM Role for GitHub Actions to access EKS

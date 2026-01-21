5e959f61424e55d0ddb7df93605416ceef6efc68
Sean Lam <seanl@seancotech.com>
2026-01-21 13:46:46 -0800

    Address tf state issues

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -6,6 +6,14 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket         = "tf-state-project-overengineer"
+    key            = "staging/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
   required_version = ">= 1.0.0"
 }
 

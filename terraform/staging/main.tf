48eb808c560fc013c29071302c5f7170408ba482
Sean Lam <seanl@seancotech.com>
2026-01-22 14:11:10 -0800

    Attempt to fix storage provisioning issues

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -69,6 +69,11 @@ resource "aws_iam_role_policy_attachment" "eks_describe" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
+# Allow EKS nodes to work with EBS CSI Driver
+resource "aws_iam_role_policy_attachment" "node_ec2" {
+  role       = module.eks.eks_managed_node_groups["ng1"].iam_role_name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicy"
+}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"

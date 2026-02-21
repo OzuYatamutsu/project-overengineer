7246315de11d9d436b0e78c2c121a659b18bc0e8
Sean Lam <seanl@seancotech.com>
2026-02-20 18:17:08 -0800

    Move provider code to providers.tf

---
+++ terraform/staging/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.13.0, < 6.0.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "tf-state-project-overengineer"
+    key            = "staging/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}

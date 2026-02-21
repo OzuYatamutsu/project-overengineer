3fb9cbb1d60ccd6b4909298bf150d8749d7482f3
Sean Lam <seanl@seancotech.com>
2026-02-21 05:44:51 -0800

    Enable general-purpose nodepool

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -82,7 +82,8 @@ module "eks" {
   enable_cluster_creator_admin_permissions = true
   create_auto_mode_iam_resources           = true
   compute_config = {
-    enabled = true
+    enabled    = true
+    node_pools = ["general-purpose"]
   }
 
   vpc_id     = module.vpc.vpc_id

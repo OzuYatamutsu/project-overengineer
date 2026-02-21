16787737e278e4ef476d1b5174ddd4578d90c97a
Sean Lam <seanl@seancotech.com>
2026-02-21 04:51:01 -0800

    Attempt to fix cni issue

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -85,6 +85,12 @@ module "eks" {
     enabled = true
   }
 
+  addons = {
+    vpc_cni = {
+      most_recent = true
+    }
+  }
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
   tags = {

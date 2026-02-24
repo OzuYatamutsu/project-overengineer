e6fac86bf07c5f11f2175dadf2926eae20243b35
Sean Lam <seanl@seancotech.com>
2026-02-23 19:55:11 -0800

    Set environment to staging

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -96,7 +96,7 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
   tags = {
-    Environment = "dev"
+    Environment = "staging"
     Terraform   = "true"
   }
 

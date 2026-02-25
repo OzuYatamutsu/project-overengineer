e1aad263ba49be71e54b82618411ad6afb029282
Sean Lam <seanl@seancotech.com>
2026-02-25 00:50:39 -0800

    Fix paths

--- terraform/environment/staging/main.tf
+++ terraform/environment/staging/main.tf
@@ -1,5 +1,5 @@
 module "aws_project_overengineer" {
-  source = "./modules/aws-project-overengineer"
+  source = "../../modules/aws-project-overengineer"
 
   environment_name = "staging"
   region           = "us-east-2"

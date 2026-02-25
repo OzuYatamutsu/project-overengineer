117a00b9f709f682c01753f7518e8b74cb6902e0
Sean Lam <seanl@seancotech.com>
2026-02-25 00:50:39 -0800

    Fix paths

--- terraform/environment/production/main.tf
+++ terraform/environment/production/main.tf
@@ -1,5 +1,5 @@
 module "aws_project_overengineer" {
-  source = "./modules/aws-project-overengineer"
+  source = "../../modules/aws-project-overengineer"
 
   environment_name = "production"
   region           = "us-east-2"

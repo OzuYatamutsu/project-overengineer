a464877c40a80014c6c37e26e31c1ad89f3f1660
Sean Lam <seanl@seancotech.com>
2026-03-06 16:00:22 -0700

    Start addressing MR comments

--- deploy-kube-cluster.sh
+++ deploy-kube-cluster.sh
@@ -32,6 +32,7 @@ kubectl apply -f status-api/service.yaml
 kubectl apply -f ocr-worker/service.yaml
 kubectl apply -f project-overengineer-fe/service.yaml
 kubectl apply -f loki/service.yaml
+kubectl apply -f alloy/daemonset.yaml
 kubectl apply -f grafana/service.yaml
 
 check_rollout statefulset redis-master 600s
@@ -42,6 +43,7 @@ check_rollout deployment status-api 600s
 check_rollout statefulset ocr-worker 600s
 check_rollout statefulset project-overengineer-fe 600s
 check_rollout statefulset loki 600s monitoring-plane
+check_rollout statefulset alloy 600s monitoring-plane
 check_rollout statefulset grafana 600s monitoring-plane
 
 kubectl get all

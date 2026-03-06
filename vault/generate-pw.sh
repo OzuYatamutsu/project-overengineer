33f66835d906493a06e50a9c4ab6b19c53f5597b
Sean Lam <seanl@seancotech.com>
2026-03-06 14:35:36 -0700

    Generate grafana-admin-password on cluster deploy

--- vault/generate-pw.sh
+++ vault/generate-pw.sh
@@ -11,3 +11,13 @@ else
       --dry-run=client -o yaml | kubectl apply -f -
   echo "Redis password generated and saved."
 fi
+
+if kubectl get secret grafana-admin-password >/dev/null 2>&1; then
+  echo "Found existing grafana password, skipping."
+else
+  echo "Generating new grafana password..."
+  kubectl create secret generic grafana-admin-password \
+      --from-literal=password="$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)" \
+      --dry-run=client -o yaml | kubectl apply -f -
+  echo "Grafana password generated and saved."
+fi

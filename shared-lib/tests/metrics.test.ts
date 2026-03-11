d84466c2164e6ee1b9589746911cb2469c3c34e8
Sean Lam <seanl@seancotech.com>
2026-03-11 19:15:59 -0400

    Fix unittest error

--- shared-lib/tests/metrics.test.ts
+++ shared-lib/tests/metrics.test.ts
@@ -2,7 +2,7 @@ import { registerCounter, startMetricsServer } from '../metrics'
 import { test, expect } from '@playwright/test'
 
 test('can register a counter', () => {
-  expect(() => registerCounter("test_counter", "A test counter")).not.toThrow()
+  expect(() => registerCounter(`test_counter_${Date.now()}`, "A test counter")).not.toThrow()
 })
 test('can start the metrics server', () => {
   expect(() => startMetricsServer(3000)).not.toThrow()

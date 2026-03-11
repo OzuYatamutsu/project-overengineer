085057bfecafc954f126e9dea61921e435a9b17b
Sean Lam <seanl@seancotech.com>
2026-03-11 19:18:37 -0400

    registerCounter should return a counter

--- shared-lib/tests/metrics.test.ts
+++ shared-lib/tests/metrics.test.ts
@@ -1,8 +1,11 @@
+import { count } from 'node:console'
 import { registerCounter, startMetricsServer } from '../metrics'
 import { test, expect } from '@playwright/test'
 
 test('can register a counter', () => {
-  expect(() => registerCounter(`test_counter_${Date.now()}`, "A test counter")).not.toThrow()
+  const counter = registerCounter(`test_counter_${Date.now()}`, "A test counter", ["label1"])
+  expect(counter).toBeDefined()
+  expect(counter.inc({"label1": "value1"})).not.toThrow()
 })
 test('can start the metrics server', () => {
   expect(() => startMetricsServer(3000)).not.toThrow()

51f9af6db8c952263603f56ab1d111a4dec71358
Sean Lam <seanl@seancotech.com>
2026-03-11 19:22:23 -0400

    Fix unittest issue

--- shared-lib/tests/metrics.test.ts
+++ shared-lib/tests/metrics.test.ts
@@ -5,7 +5,7 @@ import { test, expect } from '@playwright/test'
 test('can register a counter', () => {
   const counter = registerCounter(`test_counter_${Date.now()}`, "A test counter", ["label1"])
   expect(counter).toBeDefined()
-  expect(counter.inc({"label1": "value1"})).not.toThrow()
+  expect(() => counter.inc({"label1": "value1"})).not.toThrow()
 })
 test('can start the metrics server', () => {
   expect(() => startMetricsServer(3000)).not.toThrow()

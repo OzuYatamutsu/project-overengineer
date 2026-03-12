e560782cf31e068401d03a691da91ab3c0c9f7aa
Sean Lam <seanl@seancotech.com>
2026-03-11 20:41:36 -0400

    Add support for registering gauges

--- shared-lib/tests/metrics.test.ts
+++ shared-lib/tests/metrics.test.ts
@@ -1,10 +1,15 @@
-import { registerCounter, startMetricsServer } from '../metrics'
+import { registerCounter, registerGauge, startMetricsServer } from '../metrics'
 import { test, expect } from '@playwright/test'
 
 test('can register a counter', () => {
   const counter = registerCounter(`test_counter_${Date.now()}`, "A test counter", ["label1"])
   expect(counter).toBeDefined()
   expect(() => counter.inc({"label1": "value1"})).not.toThrow()
+})
+test('can register a gauge', () => {
+  const gauge = registerGauge(`test_gauge_${Date.now()}`, "A test gauge", ["label1"])
+  expect(gauge).toBeDefined()
+  expect(() => gauge.set({"label1": "value1"}, 42)).not.toThrow()
 })
 test('can start the metrics server', () => {
   expect(() => startMetricsServer(3000)).not.toThrow()

91f17bf114b2b2ff7a493c30d91c9313af6decf0
Sean Lam <seanl@seancotech.com>
2026-03-24 10:16:44 -0400

    Fix incorrect route

--- project-overengineer-fe/tests/metrics/metrics.test.ts
+++ project-overengineer-fe/tests/metrics/metrics.test.ts
@@ -1,9 +1,9 @@
 import { test, expect, request } from '@playwright/test'
 
 test.describe('Metrics API', () => {
-    test('should return 200 OK for /api/v1/metrics', async ({ baseURL }) => {
+    test('should return 200 OK for /metrics', async ({ baseURL }) => {
         const reqContext = await request.newContext()
-        const response = await reqContext.get(`${baseURL}/api/v1/metrics`)
+        const response = await reqContext.get(`${baseURL}/metrics`)
 
         expect(response.status()).toBe(200)
     })

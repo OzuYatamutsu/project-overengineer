ba9dc107cbd5f42cc856977c631122b20e9f4da5
Sean Lam <seanl@seancotech.com>
2026-03-24 10:21:32 -0400

    Attempt to fix /api/metrics unit tests

--- project-overengineer-fe/tests/api/metrics.test.ts
+++ project-overengineer-fe/tests/api/metrics.test.ts
@@ -1,9 +1,9 @@
 import { test, expect, request } from '@playwright/test'
 
 test.describe('Metrics API', () => {
-    test('should return 200 OK for /metrics', async ({ baseURL }) => {
+    test('should return 200 OK for /api/metrics', async ({ baseURL }) => {
         const reqContext = await request.newContext()
-        const response = await reqContext.get(`${baseURL}/metrics`)
+        const response = await reqContext.get(`${baseURL}/api/metrics`)
 
         expect(response.status()).toBe(200)
     })

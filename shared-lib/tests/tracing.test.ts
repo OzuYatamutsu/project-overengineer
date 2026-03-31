f008630314bd57c1184cacaa6a10238733ba47c9
Sean Lam <seanl@seancotech.com>
2026-03-31 19:11:10 -0400

    Get tracer object

--- shared-lib/tests/tracing.test.ts
+++ shared-lib/tests/tracing.test.ts
@@ -1,8 +1,15 @@
-import { initTracing, shutdownTracing } from '../tracing'
+import { initTracing, shutdownTracing, getTracer } from '../tracing'
 import { test, expect } from '@playwright/test'
 
 test('can init and shutdown tracing', async () => {
     await initTracing()
     await shutdownTracing()
     expect(true).toBeTruthy()
+})
+
+test('can get tracer', async () => {
+    await initTracing()
+    const tracer = getTracer('shared-lib')
+    expect(tracer).toBeDefined()
+    await shutdownTracing()
 })

4020cb3cb88d4e7ccbebf9f0be2d44609232997a
Sean Lam <seanl@seancotech.com>
2026-04-01 02:55:43 -0400

    Fix build issue

--- shared-lib/tracing.ts
+++ shared-lib/tracing.ts
@@ -2,8 +2,8 @@ import { NodeSDK } from "@opentelemetry/sdk-node"
 import { trace, Tracer } from "@opentelemetry/api"
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node"
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc"
-import { Resource } from "@opentelemetry/resources"
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions"
+import { resourceFromAttributes } from "@opentelemetry/resources"
+import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions"
 
 const traceExporter = new OTLPTraceExporter({
     url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT || "http://localhost:4317",
@@ -15,9 +15,9 @@ export async function initTracing(serviceName: string): Promise<void> {
     sdk = new NodeSDK({
         traceExporter,
         instrumentations: [getNodeAutoInstrumentations()],
-        resource: new Resource({
-            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-        })
+        resource: resourceFromAttributes({
+            [ATTR_SERVICE_NAME]: serviceName,
+        }),
     })
     await sdk.start()
 }

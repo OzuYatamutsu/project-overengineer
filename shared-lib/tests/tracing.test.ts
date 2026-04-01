1c27d28436179780dc2d02b2003f16fe3812b0c7
Sean Lam <seanl@seancotech.com>
2026-04-01 02:58:32 -0400

    Fix tests

--- shared-lib/tests/tracing.test.ts
+++ shared-lib/tests/tracing.test.ts
@@ -2,13 +2,13 @@ import { initTracing, shutdownTracing, getTracer } from '../tracing'
 import { test, expect } from '@playwright/test'
 
 test('can init and shutdown tracing', async () => {
-    await initTracing()
+    await initTracing("shared-lib")
     await shutdownTracing()
     expect(true).toBeTruthy()
 })
 
 test('can get tracer', async () => {
-    await initTracing()
+    await initTracing("shared-lib")
     const tracer = getTracer('shared-lib')
     expect(tracer).toBeDefined()
     await shutdownTracing()

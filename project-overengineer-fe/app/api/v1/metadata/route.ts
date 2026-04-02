8796f41c9287fe0418c9e55962c71ec142027b4b
Sean Lam <seanl@seancotech.com>
2026-04-02 14:37:16 -0400

    Fix unittest failures

--- project-overengineer-fe/app/api/v1/metadata/route.ts
+++ project-overengineer-fe/app/api/v1/metadata/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 /*
  * Used to get current url of status api
  */
-export async function GET(request: Request): Promise<NextResponse> {
+export async function GET(_request: Request): Promise<NextResponse> {
   return NextResponse.json({
     statusApiUrl: process.env.STATUS_API_URL
   })

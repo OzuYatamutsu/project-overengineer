720f9c80b45f68c9affa0c55b21113f0984c8723
Sean Lam <seanl@seancotech.com>
2026-04-04 14:51:52 -0400

    Integration testing skeleton

--- integration-tests/playwright.config.ts
+++ integration-tests/playwright.config.ts
@@ -1,11 +1,3 @@
 import { defineConfig } from '@playwright/test'
 
-export default defineConfig({
-  webServer: {
-    command: 'TS_NODE_TRANSPILE_ONLY=1 ts-node worker.ts',
-    reuseExistingServer: false,
-    env: {},
-    stdout: 'pipe',
-    stderr: 'pipe'
-  },
-})
+export default defineConfig({})

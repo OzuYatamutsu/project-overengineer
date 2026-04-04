05cbd6a937e37a0a38a8295240b8f30e3c28a93e
Sean Lam <seanl@seancotech.com>
2026-04-04 14:51:52 -0400

    Integration testing skeleton

---
+++ integration-tests/tests/e2e.test.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test'
+
+test("full image processing pipeline should work", async () => {
+    expect(true).toBeTruthy()  // TODO: implement actual tests
+})

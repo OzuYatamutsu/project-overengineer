d7b38814a4ca4e0a600c249bcae2a71eee1621b1
Sean Lam <seanl@seancotech.com>
2026-04-05 11:14:52 -0400

    Correct kubectl command in integration-tests

--- integration-tests/utils/kubectl-calls.ts
+++ integration-tests/utils/kubectl-calls.ts
@@ -2,11 +2,11 @@ import { execSync } from 'node:child_process'
 
 export function getFeEndpointFromKubectl(): string {
     const hostname = execSync(
-        'kubectl get svc -n default -l app=project-overengineer-fe -o jsonpath="{.items[0].status.loadBalancer.ingress[0].hostname}"'
+        'kubectl get svc -n default -l app=project-overengineer-fe -o jsonpath="{.status.loadBalancer.ingress[0].hostname}"'
     ).toString().trim()
 
     const port = execSync(
-        'kubectl get svc -n default -l app=project-overengineer-fe -o jsonpath="{.items[0].spec.ports[0].port}"'
+        'kubectl get svc -n default -l app=project-overengineer-fe -o jsonpath="{.spec.ports[?(@.name=="http")].port}"'
     ).toString().trim()
 
     return `${hostname}:${port}`

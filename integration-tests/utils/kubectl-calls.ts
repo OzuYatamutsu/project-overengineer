4aafe7feedc9a47ec4a954eabeb21d9b30da4bb6
Sean Lam <seanl@seancotech.com>
2026-04-05 14:03:42 -0400

    Parse kube state a different way

--- integration-tests/utils/kubectl-calls.ts
+++ integration-tests/utils/kubectl-calls.ts
@@ -2,11 +2,11 @@ import { execSync } from 'node:child_process'
 
 export function getFeEndpointFromKubectl(): string {
     const hostname = execSync(
-        'kubectl get svc -n default -l app=project-overengineer-fe -o jsonpath="{.status.loadBalancer.ingress[0].hostname}"'
+        'kubectl get svc svc-project-overengineer-fe -o jsonpath="{.status.loadBalancer.ingress[0].hostname}"'
     ).toString().trim()
 
     const port = execSync(
-        'kubectl get svc -n default -l app=project-overengineer-fe -o jsonpath="{.spec.ports[?(@.name=="http")].port}"'
+        'kubectl get svc svc-project-overengineer-fe -o jsonpath="{.spec.ports[?(@.name=="http")].port}"'
     ).toString().trim()
 
     return `${hostname}:${port}`

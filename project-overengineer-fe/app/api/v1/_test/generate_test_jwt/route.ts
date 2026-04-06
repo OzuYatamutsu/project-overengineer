bce5a8eddd3d9ac4302728d03865cb4e6b504003
Sean Lam <seanl@seancotech.com>
2026-04-06 00:33:31 -0400

    Fix build error

--- project-overengineer-fe/app/api/v1/_test/generate_test_jwt/route.ts
+++ project-overengineer-fe/app/api/v1/_test/generate_test_jwt/route.ts
@@ -7,14 +7,23 @@ const TEST_IMAGE_HASH = "8acedaae7dcd5cc3358dac05c4b0a071ef12385a41152571d2211db
 // /api/v1/_test/generate_test_jwt is hit by integration tests to generate a
 // valid JWT only when the specified test image is provided.
 export async function POST(request: Request): Promise<NextResponse> {
-    const jobId = request.headers.get("x-test-job-id")
+    const jobId = request.headers.get("x-test-job-id") || ""
     const rawImageData = await request.arrayBuffer()
     const hash = createHash('sha256').update(Buffer.from(rawImageData)).digest('hex')
 
     if (hash !== TEST_IMAGE_HASH) {
         return NextResponse.json({
             message: 'Invalid test image',
-            jobId: "",
+            jobId: jobId,
+        }, {
+            status: 400
+        })
+    }
+
+    if (!jobId) {
+        return NextResponse.json({
+            message: 'Missing x-test-job-id header',
+            jobId: jobId,
         }, {
             status: 400
         })

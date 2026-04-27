318ad25b98bf2c2682a7f42a3094664bcc0d0271
Sean Lam <seanl@seancotech.com>
2026-04-27 11:47:48 -0400

    Fix deploy issue

--- terraform/modules/pagerduty-project-overengineer/main.tf
+++
@@ -1,9 +0,0 @@
-resource "pagerduty_service" "project-overengineer-fe" {
-  name              = "project-overengineer-fe"
-  escalation_policy = pagerduty_escalation_policy.fe.id
-
-  incident_urgency_rule {
-    type = "constant"
-    urgency = "high"
-  }
-}
--- terraform/modules/pagerduty-project-overengineer/providers.tf
+++ terraform/modules/pagerduty-project-overengineer/providers.tf
@@ -8,5 +8,3 @@ terraform {
 
   required_version = ">= 1.0.0"
 }
-
-provider "pagerduty" {}

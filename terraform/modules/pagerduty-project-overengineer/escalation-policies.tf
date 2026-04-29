0d0bcabff2fabd1658f289becaf302ddee252f4c
Sean Lam <seanl@seancotech.com>
2026-04-29 14:14:00 -0400

    Fill in all escalation policies

--- terraform/modules/pagerduty-project-overengineer/escalation-policies.tf
+++ terraform/modules/pagerduty-project-overengineer/escalation-policies.tf
@@ -11,3 +11,45 @@ resource "pagerduty_escalation_policy" "fe" {
     }
   }
 }
+
+resource "pagerduty_escalation_policy" "ocr" {
+  name = "OCR"
+  num_loops = 2
+
+  rule {
+    escalation_delay_in_minutes = 5
+
+    target {
+      type = "user_reference"
+      id   = pagerduty_user.seanl_seancotech_com.id
+    }
+  }
+}
+
+resource "pagerduty_escalation_policy" "storage" {
+  name = "Storage"
+  num_loops = 2
+
+  rule {
+    escalation_delay_in_minutes = 5
+
+    target {
+      type = "user_reference"
+      id   = pagerduty_user.seanl_seancotech_com.id
+    }
+  }
+}
+
+resource "pagerduty_escalation_policy" "observability" {
+  name = "Observability"
+  num_loops = 2
+
+  rule {
+    escalation_delay_in_minutes = 5
+
+    target {
+      type = "user_reference"
+      id   = pagerduty_user.seanl_seancotech_com.id
+    }
+  }
+}
